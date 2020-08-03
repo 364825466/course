c1863ec697fb015208d0e0e7b1e3268e70e580fa
364825466 <364825466@qq.com>
2020-08-03 23:47:56 +0800

    1.解决Eureka启动失败问题 2.日志输出优化

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,29 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+	// springboot默认继承slf4j框架，它定义了一些日志接口，规范日志输入，
+	// 真正的实现是logback或log4j,我们代码中写的都是slf4j的代码
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
 
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
 }
