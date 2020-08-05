cebb1e83f6022f6e62ebf492ddd0e6a99c2f08e2
364825466 <364825466@qq.com>
2020-08-05 23:01:36 +0800

    新增gateway子模块，增加gateway依赖，并注册到注册中心

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.course.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	// springboot默认继承slf4j框架，它定义了一些日志接口，规范日志输入，
+	// 真正的实现是logback或log4j,我们代码中写的都是slf4j的代码
+	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+}
